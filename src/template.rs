6112812e097fb868f914051ddb47edca0a5973b5
Kyle Harris <kaharrs2@gmail.com>
2018-12-01 13:29:35 -0700

    Added a quick module template

---
+++ src/template.rs
@@ -0,0 +1,27 @@
+pub mod template {
+
+    pub fn part1() -> i32 {
+        println!("Not yet implemented!");
+        0
+    }
+
+    pub fn part2() -> i32 {
+        println!("Not yet implemented!");
+        0
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn part1examples() {
+            assert_eq!(part1(), 0);
+        }
+
+        #[test]
+        fn part2examples() {
+            assert_eq!(part2(), 0);
+        }
+    }
+}
